ff84723bd55ed846a3577d7e79c3687d93102632
Daniel Busuioc <daniel.busuioc@neovision.dev>
2022-08-31 12:04:02 +0300

    first commit

---
+++ app/Services/Newsletter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use MailchimpMarketing\ApiClient;
+
+class Newsletter
+{
+    public function subscribe(string $email)
+    {
+        $mailchimp = new ApiClient();
+
+        $mailchimp->setConfig([
+            'apiKey' => config('services.mailchimp.key'),
+            'server' => 'us8'
+        ]);
+
+        return $mailchimp->lists->addListMember(config('services.mailchimp.lists.subscribers'), [
+            'email_address' => $email,
+            'status' => 'subscribed'
+        ]);
+    }
+}
